613d1ab8afba7917618f5ad519747a4ca947db87
Brian Chung & Hank Chen <pair+brianjaychung.hank860502@devbootcamp.com>
2016-06-21 11:33:29 -0700

    append object

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,16 @@ $.ready(function(){
 
   $.on('a','click', function(e){
     e.preventDefault();
+    $.request({
+      url: "http://spa-badge-api.herokuapp.com/teachers/9",
+      type: "GET",
+      data: 'json'
+    }).then(function(data){
+      var walker = JSON.parse(data)
+      debugger;
+      var textnode = document.createTextNode(walker)
+      $.select('a')[0].parentElement.appendChild(textnode);
+    })
   })
 })
 
