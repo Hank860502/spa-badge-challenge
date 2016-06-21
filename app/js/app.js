a4da5a887c39aa19d446944bd8032131a27b60cc
Brian Chung & Hank Chen <pair+brianjaychung.hank860502@devbootcamp.com>
2016-06-21 16:14:46 -0700

    first try to handle bars

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,18 @@
 $.ready(function(){
+  $.request({
+      url: "http://spa-badge-api.herokuapp.com/teachers",
+      type: "GET",
+      data: 'json'
+    }).then(function(data){
+      var teachers = JSON.parse(data)
+      var theTemplateScript = $.select("#teachers").innerHTML;
+      var theTemplate = Handlebars.compile(theTemplateScript);
+      var context ={ teachers:teachers }
+      var theCompiledHtml = theTemplate(context);
+      debugger;
+      $.select('.content-placeholder')[0].innerHTML= theCompiledHtml;
+
+  })
 
   $.on('a','click', function(e){
     e.preventDefault();
@@ -14,7 +28,8 @@ $.ready(function(){
         teacherPhrase.push(badges[i].phrase);
       }
       var textnode = document.createTextNode(teacherPhrase)
-      debugger;
+      var node = document.createElement("br")
+      $.select('a')[0].parentElement.appendChild(node);
       $.select('a')[0].parentElement.appendChild(textnode);
     })
   })
