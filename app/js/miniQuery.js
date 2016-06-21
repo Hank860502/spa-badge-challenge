0a32769b284fe31cd0288c91ffe90c6db20ab9d3
Brian Chung & Hank Chen <pair+brianjaychung.hank860502@devbootcamp.com>
2016-06-21 10:32:50 -0700

    include mini query

---
+++ app/js/miniQuery.js
@@ -0,0 +1,122 @@
+/*!
+ * minQuery
+ */
+var SweetSelector = (function() {
+  return {
+    select:function(target) {
+      if(target[0] === "#"){
+        target = target.substring(1)
+        return document.getElementById(target);
+      }else if(target[0] === "."){
+        target = target.substring(1)
+        return document.getElementsByClassName(target)[0]
+      }else{
+        return document.getElementsByTagName(target)[0];
+      }
+    }
+  };
+})()
+
+var DOM = (function(){
+  return{
+    hide:function(target){
+      SweetSelector.select(target).style.visibility = "hidden";
+    },
+    show:function(target){
+      SweetSelector.select(target).style.visibility = "visible";
+    },
+    addClass:function(target, newClass){
+      SweetSelector.select(target).className += (' ' + newClass)
+    },
+    removeClass:function(target, deleteClass){
+      SweetSelector.select(target).className.replace( /(?:^|\s)deleteClass(?!\S)/g , '' )
+    }
+  }
+})()
+
+var EventDispatcher = (function(){
+  return{
+    on:function(target,event,action){
+      SweetSelector.select(target).addEventListener(event,action)
+    },
+    trigger:function(target,event){
+      var e = new Event(event)
+      SweetSelector.select(target).dispatchEvent(e);
+    }
+  }
+})()
+
+var AjaxWrapper = (function(){
+
+  // function reqListener() {
+  //   console.log(this.responseText);
+  // }
+  return{
+    request:function(info){
+      var link = info.url;
+      var linkType = info.type;
+      return new Promise(function(resolve, reject) {
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.open(linkType, link);
+        xmlhttp.onload = function() {
+          if (xmlhttp.status == 200) {
+          resolve(xmlhttp.responseText);
+          }
+          else{
+          reject('something else other than 200 was returned');
+          }
+        };
+      xmlhttp.send();
+      })
+    }
+  }
+
+})()
+
+
+var $ = (function(){
+  return{
+    select: SweetSelector.select,
+    hide: DOM.hide,
+    show: DOM.show,
+    addClass: DOM.addClass,
+    removeClass: DOM.removeClass,
+    on: EventDispatcher.on,
+    trigger: EventDispatcher.trigger,
+    request: AjaxWrapper.request,
+    ready: function(callback){
+      document.addEventListener("DOMContentLoaded", function(event){
+        callback()
+      })
+    }
+  }
+
+})()
+
+
+// $.ready( function() { console.log("The DOM is ready"); })
+// $.hide('.klass')
+
+
+// console.log(miniQuery.DOM.addClass('.klass', 'shadi'))
+// miniQuery.EventDispatcher.on('.klass', 'Andrew', function() { console.log("awesome") });
+// miniQuery.EventDispatcher.trigger('.klass', 'Andrew')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
